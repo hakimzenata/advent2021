d6773695ce98cc5e044dd7489d84a515ef182319
hakimzenata <hakim1bal@protonmail.com>
2021-12-20 03:18:38 +0100

    day3 part1

--- day3/c++/solve.cpp
+++ day3/c++/solve.cpp
@@ -6,20 +6,19 @@
 
 #define bits 12
 
-using namespace std;
 
-vector<bitset<bits>> readFile(string fileName) {
-    ifstream file(fileName);
-    vector<bitset<bits>> lines;
-    string line;
-    while (file >> line) {
-	lines.push_back(bitset<bits>(line));
-    }
-    return lines;
+std::vector<std::bitset<bits>> readFile(std::string fileName) {
+	std::ifstream file(fileName);
+	std::vector<std::bitset<bits>> lines;
+	std::string line;
+	while (file >> line) {
+		lines.push_back(std::bitset<bits>(line));
+	}
+	return lines;
 }
 
-vector<int32_t> part1(vector<bitset<bits>> lines) {
-    bitset<bits> epsilon , gamma;
+std::vector<int32_t> part1(std::vector<std::bitset<bits>> lines) {
+    std::bitset<bits> epsilon , gamma;
     for (int i = bits -1 ; i >= 0; i--) {
 	    int counts[2] = {0,0};
 	    for (auto line : lines) {
@@ -27,8 +26,8 @@ vector<int32_t> part1(vector<bitset<bits>> lines) {
 	    }
 	    gamma[i] = counts[0] > counts[1]? 0 : 1;
     }
-    epsilon =  bitset<bits>(gamma).flip();
-    vector<int32_t> result;
+    epsilon =  std::bitset<bits>(gamma).flip();
+    std::vector<int32_t> result;
     result.push_back(gamma.to_ulong());
     result.push_back(epsilon.to_ulong());
     return result;
@@ -36,14 +35,14 @@ vector<int32_t> part1(vector<bitset<bits>> lines) {
 }
 
 int main(int argc, char *argv[]) {
-    vector<bitset<bits>> lines = readFile(argv[1]);
-    vector<int32_t> result = part1(lines);
-    cout << "------------------" << endl;
-    cout << "Part 1: " << endl;
-    cout << "------------------" << endl;
-    cout << "epsilon: " << result[1] << endl;
-    cout << "gamma: " << result[0] << endl;
-    cout << "power consumption: " << result[0] * result[1] << endl;
+    std::vector<std::bitset<bits>> lines = readFile(argv[1]);
+    std::vector<int32_t> result = part1(lines);
+    std::cout << "------------------" << std::endl;
+    std::cout << "Part 1: " << std::endl;
+    std::cout << "------------------" << std::endl;
+    std::cout << "epsilon: " << result[1] << std::endl;
+    std::cout << "gamma: " << result[0] << std::endl;
+    std::cout << "power consumption: " << result[0] * result[1] << std::endl;
     return 0;
 }	
 
